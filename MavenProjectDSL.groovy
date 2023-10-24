c08c641881efafcb0fc252c2681948d5ef8b3112
Chenphop Chanphum <82653803+chenphopp@users.noreply.github.com>
2023-10-24 23:44:56 +0700

    Update MavenProjectDSL.groovy

--- MavenProjectDSL.groovy
+++ MavenProjectDSL.groovy
@@ -7,9 +7,6 @@ job('First-Maven-Project-Via-DSL') {
         scm('* * * * *')
     }
     steps {
-        // Set Git user email and name
-        sh 'git config --global user.email "chenphop.ch@gmail.com"'
-        sh 'git config --global user.name "chenphopp"'
         maven('clean package', 'maven-samples/single-module/pom.xml')
     }
     publishers {
